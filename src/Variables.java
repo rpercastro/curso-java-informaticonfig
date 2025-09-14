9f9d82c9b6881aa430a4e603a0e7a6f8ab746c8a
Roberto Perez Castro <rpercastro@gmail.com>
2025-09-14 19:53:38 +0100

    Variables

---
+++ src/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        String nombre = "Hola Mundo desde Java";
+        int valor = 100;
+        double valor2 = 10.5;
+        char letra1 = 'B';
+        boolean esEstudiante = true;
+
+        System.out.println(nombre);
+        System.out.println(valor);
+        System.out.println(valor2);
+        System.out.println(letra1);
+        System.out.println(esEstudiante);
+    }
+}
